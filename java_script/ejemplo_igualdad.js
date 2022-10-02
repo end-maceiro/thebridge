36a5e4b97e2f09553639dc820060fc4bc62f6572
Ezequiel Nicolás Diaz Maceiro <diaz.maceiro@icloud.com>
2022-10-03 00:28:53 +0200

    act

---
+++ java_script/ejemplo_igualdad.js
@@ -0,0 +1,10 @@
+
+    var n = 9;
+    var n2 = 9;
+    console.log(n == n2); // Devuelve true porque son iguales los contenidos de las variables
+    var n3 = 109;
+    console.log(n == n3); // Devuelve false porque no son iguales los contenidos de las variables
+    var n4 = '9';
+    console.log(n == n4);// Devuelve true porque son iguales los contenidos de las variables aunque no los tipos
+    var n5 = 9.0;
+    console.log(n == n5);// Devuelve true porque son iguales los contenidos de las variables aunque no los tipos
---
+++ java_script/ejercicio_1.js
@@ -0,0 +1,29 @@
+
+/*function suma(num1,num2) {
+    var num1;
+    var num2;
+    var resultado = num1 + num2;
+    console.log(resultado);
+}
+suma(2,2)*/
+
+
+function diferenciaFechas () {
+
+//let diaDeNacimiento = parseInt(prompt("Dia de cumpleaños"));
+//let mesDeNacimiento = parseInt(prompt("Mes de Nacimiento"));
+var añoDeNacimiento = parseInt(1985);
+
+//let diaActual = parseInt(prompt("Que dia es hoy:"));
+//let mesActual = parseInt(prompt("El mes actual"));
+var añoActual = parseInt(2022);
+
+//let diaActual = parseInt(prompt("Que dia es hoy:"));
+//let mesActual = parseInt(prompt("El mes actual"));
+var dif_año = parseInt(añoActual - añoDeCumpleaños);
+
+
+    if (dif_año < 17){
+        console.log();
+    }
+}
